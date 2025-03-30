3afc788ca25c77c68fc52b76fcde28bdcfbea9dc
Pankaj <pankaj2007acharjee@gmail.com>
2025-03-30 23:45:17 +0530

    User card message for chat building

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,14 @@
+import Agent from '@/components/Agent'
+import React from 'react'
+
+const Page = () => {
+    return (
+        <div>
+            Interview Page
+
+            <Agent userName="You" userId="user1" type="generate" />
+        </div>
+    )
+}
+
+export default Page
---
+++ components/Agent.tsx
@@ -0,0 +1,85 @@
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import React from 'react'
+
+enum CallStatus {
+    INACTIVE = 'INACTIVE',
+    CONNECTING = 'CONNECTING',
+    ACTIVE = 'ACTIVE',
+    FINISHED = 'FINISHED'
+}
+
+
+
+const Agent = ({ userName }: AgentProps) => {
+
+    const callStatus = CallStatus.FINISHED
+    const isSpeaking = true
+
+    const messages = [
+        'Whats your age?',
+        'Do you have a pet ?'
+    ]
+
+    const lastMessage = messages[messages.length - 1]
+
+    return (
+        <>
+            <div className='call-view'>
+                <div className='card-interviewer'>
+                    <div className='avatar'>
+                        <Image src="/Logo-rem.png" alt='vapi-voiceAgent'
+                            width={65} height={54}
+                            className='object-cover' />
+                        {isSpeaking && <span className='animate-speak' />}
+                    </div>
+                    <h3>Interviewer (Agent)</h3>
+                </div>
+
+                {/* User profile card */}
+                <div className='card-border'>
+                    <div className='card-content'>
+                        <Image src='/user-avatar.png' alt='user-avatar' width={540} height={540}
+                            className='object-cover rounded-full size-[120px]'
+                        />
+
+                        <h3>{userName}</h3>
+                    </div>
+                </div>
+            </div>
+
+
+            {messages.length > 0 && (
+                <div className='transcript-border'>
+                    <div className='transcript'>
+                        <p key={lastMessage} className=''>
+                            {lastMessage}
+                        </p>
+                    </div>
+                </div>
+            )}
+
+
+            <div className='w-full flex justify-center'>
+                {callStatus !== 'ACTIVE' ? (
+                    <button className='relative btn-call'>
+                        <span className={cn('absolute animate-ping rounded-full opacity-65', callStatus !== 'CONNECTING' & 'hidden')} />
+
+
+
+                        <span>
+                            {callStatus === 'INACTIVE' || callStatus === 'FINISHED' ? 'Call' : '...'}
+                        </span>
+                    </button>
+                ) : (
+                    <button className='btn-disconnect'>
+                        End
+                    </button>
+                )}
+            </div>
+        </>
+
+    )
+}
+
+export default Agent
